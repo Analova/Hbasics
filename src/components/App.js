a52d7e0ebe86603f00508948f63bb354def7ce80
Ana Nikolova <ani.nik19@gmail.com>
2020-12-21 16:36:07 +0100

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,13 @@
 import React, { useState } from "react";
 
 import ResourceList from "./ResourceList";
-
-// class App extends React.Component {
-//   state = {
-//     resourceName: 'posts'
-//   }
-
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <button onClick={() => this.setState({ resourceName: 'posts' })}>Posts</button>
-//         <button onClick={() => this.setState({ resourceName: 'todos' })}>Todos</button>
-//         <ResourceList resourceName={this.state.resourceName}/>
-//       </React.Fragment>
-//     )
-//   }
-// }
+import Users from "./Users";
 
 const App = () => {
   const [resourceName, setResourceName] = useState("posts");
   return (
     <React.Fragment>
+      <Users />
       <button onClick={() => setResourceName("posts")}>Posts</button>
       <button onClick={() => setResourceName("todos")}>Todos</button>
       <ResourceList resourceName={resourceName} />
